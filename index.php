ba0961de6422d497c8068c5465aa1d437c83259b
AndreyChulov <andreyc253@gmail.com>
2025-01-10 22:02:02 +0300

    Initial project status

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php ?>
+<html lang="En-En">
+    <head>
+        <meta charset="UTF-8" >
+        <title>Combats</title>
+        <link rel="icon" href="Images/icon.svg">
+    </head>
+    <body>
+        <h1>Hello user!!!</h1>
+    </body>
+</html>
