29b2c656ace8eb2635a69b08ebc1e0a18400dc1d
Grokes <default1one@gmail.com>
2025-01-24 16:45:47 +0300

    php.ini added

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php ?>
 <html lang="En-En">
-    <head>
-        <meta charset="UTF-8" >
-        <title>Combats</title>
-        <link rel="icon" href="Images/icon.svg">
-    </head>
-    <body>
-        <h1>Hello user!!!</h1>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Combats</title>
+    <link rel="icon" href="Images/icon.svg">
+</head>
+
+<body>
+    <h1>Hello user!!!</h1>
+    <?php phpinfo();
+    $a = 1;
+    $b = "fdsf"
+
+        ?>
+</body>
+
 </html>
